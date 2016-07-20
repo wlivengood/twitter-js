8a6b9a25f024162a8ded269d2340e7621e5203d8
Will Livengood <wlivengo@gmail.com>
2016-07-19 21:09:26 -0400

    added nodemon, updated start script, updated middleware

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var express = require('express');
+var chalk = require ('chalk');
 
 var app = express();
 
+app.use(function(req, res, next) {
+	console.log(chalk.red(req.method) + ' ' + chalk.blue(req.path) + ' ' + chalk.green(res.statusCode));
+	next();
+});
+
+app.use("/special/",function(req, res, next) {
+	res.send("Aren't you special?");
+	next();
+});
+
 app.get("/", function(req, res) {
 	res.send("Welcome!");
-})
+});
+
+app.get("/news", function(req, res) {
+	res.send("Here's the news");
+});
 
 app.listen(3000, function() {
 	console.log("server listening");
