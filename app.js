3666d2730ae7e05c5fb1c12e9a55e4cf1b85e30c
Will Livengood <wlivengo@gmail.com>
2016-08-03 12:29:06 -0400

    got socket.io working. TODO: update DOM on newTweet event

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var chalk = require ('chalk');
 var swig = require('swig');
 var app = express();
+var socketio = require('socket.io');
 
 app.engine('html', swig.renderFile);
 app.set("view engine", "html");
@@ -9,7 +10,6 @@ app.set("views", __dirname + '/views');
 swig.setDefaults({ cache: false });
 
 var routes = require('./routes/');
-app.use('/', routes);
 
 app.use(express.static('public'));
 
@@ -18,6 +18,7 @@ app.use(function(req, res, next) {
 	next();
 });
 
-app.listen(3000, function() {
-	console.log("server listening");
-});
+var server = app.listen(3000);
+var io = socketio.listen(server);
+
+app.use( '/', routes(io) );
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,34 @@
-var express = require('express');
-var router = express.Router();
-var tweetBank = require('../tweetBank');
-var bodyParser = require('body-parser');
+module.exports = function (io) { 
+  var express = require('express');
+  var router = express.Router();
+  var tweetBank = require('../tweetBank');
+  var bodyParser = require('body-parser');
 
-router.get('/', function (req, res) {
-  var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
-});
+  router.use(bodyParser());
 
-router.get('/users/:name', function(req, res) {
-  var name = req.params.name;
-  var list = tweetBank.find( {name: name} );
-  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list, showForm: true, autoPopName: name} );
-});
+  router.get('/', function (req, res) {
+    var tweets = tweetBank.list();
+    res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
+  });
 
-router.get('/tweets/:id', function(req, res) {
-	var id = Number(req.params.id);
-	var tweet = tweetBank.find( {id: id});
-	res.render( 'index', { title: 'Twitter.js - Post #'+id, tweets: tweet } );
-});
+  router.get('/users/:name', function(req, res) {
+    var name = req.params.name;
+    var list = tweetBank.find( {name: name} );
+    res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list, showForm: true, autoPopName: name} );
+  });
 
-router.post('/tweets', function(req, res) {
-  var name = req.body.name;
-  var content = req.body.content;
-  tweetBank.add(name, content);
-  res.redirect('/');
-});
+  router.get('/tweets/:id', function(req, res) {
+    var id = Number(req.params.id);
+    var tweet = tweetBank.find( {id: id});
+    res.render( 'index', { title: 'Twitter.js - Post #'+id, tweets: tweet } );
+  });
 
-module.exports = router;
+  router.post('/tweets', function(req, res) {
+    var name = req.body.name;
+    var content = req.body.content;
+    tweetBank.add(name, content);
+    io.sockets.emit('newTweet', {name: name, content: content});
+    res.redirect('/');
+  });
+  return router;
+};
